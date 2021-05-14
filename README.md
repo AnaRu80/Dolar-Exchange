bc29a4d724e5eeeaa31d912f701ddbc6fa9aa17c
Ana Ruiz <ruizana80@gmail.com>
2021-05-14 00:07:10 -0500

    Readme update

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
-# vuetify-dolar
+# CONTEXT AND GOLAD
 
-## Project setup
+this web app get through an Api Call using axios the exchange of the Dolar to Peso Chileno
+
+![picture alt](./src/assets/vuetify-dolar-screencapture.png "Title is optional")
+
+# Main Tecnologies
+
+## Vue
+
+- A framework for building web Aplications
+
+## Vuex
+
+- State managment for Vue
+
+## Axios
+
+For API consuming
+
+## Vuetify
+
+Framework to build rich and engaging user experiences.
+
+# Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Cambio de Dolar a Peso chileno</h1>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-date-picker
+            full-width
+            locale="es-419"
+            v-model="fecha"
+            :min="minimo"
+            :max="maximo"
+            @change="getDolar(fecha)"
+          ></v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            {{ valor }} - Peso Chileno<br />
+            {{ fecha }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+
+  data() {
+    return {
+      fecha: new Date().toISOString().substr(0, 10),
+      minimo: "1984",
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null,
+    };
+  },
+  created() {
+    this.getDolar(this.fecha);
+  },
+
+  methods: {
+    ...mapMutations(["mostrarLoading", "ocultarLoading"]),
+    async getDolar(dia) {
+      let arrayFecha = dia.split(["-"]);
+      let ddmmyy = arrayFecha[2] + "-" + arrayFecha[1] + "-" + arrayFecha[0];
+      try {
+        this.mostrarLoading({
+          titulo: "Accediendo a información",
+          color: "secondary",
+        });
+        let datos = await axios.get(
+          `https://mindicador.cl/api/dolar/${ddmmyy}`
+        );
+        if (datos.data.serie.length > 0) {
+          this.valor = datos.data.serie[0].valor;
+        } else {
+          this.valor = "sin resultados";
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.ocultarLoading();
+      }
+    },
+  },
+};
 </script>
